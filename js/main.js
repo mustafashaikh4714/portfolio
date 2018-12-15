c36fe9f073efbb5698b7dfe2178ebcea4967e877
kiranbhalerao123 <daghalerahul0@gmail.com>
2018-12-15 22:47:24 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    window.location.href = "#";
+}
+
+// document.getElementById("a").onclick = function () {
+//     document.getElementById('navi-toggle').checked = false;
+// };
+// document.getElementById("p").onclick = function () {
+//     document.getElementById('navi-toggle').checked = false;
+// };
+
+// document.getElementById("c").onclick = function () {
+//     document.getElementById('navi-toggle').checked = false;
+// };
